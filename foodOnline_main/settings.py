d5aace03028d12218ac34eb5097e83d21e125965
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-07-15 19:51:16 +0530

    Added custom user model

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+AUTH_USER_MODEL='accounts.user'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
