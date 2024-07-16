82e736083a41efa9caeb080cd22b2771e71485d1
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-07-16 11:33:22 +0530

    Added profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
@@ -9,4 +9,5 @@ class CustomUserAdmin(UserAdmin):
     list_filter=()
     fieldsets=()
 
-admin.site.register(User,CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
