180b3b631a94ccbe5ec0422bb6a43cc4f8ad5476
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-07-17 00:10:01 +0530

    Added logic registration form

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,19 @@ def registerUser(request):
     if request.method=="POST":
         form=UserForm(request.POST)
         if form.is_valid():
-            user=form.save(commit=False)
+            # password=form.cleaned_data['password']
+            # user=form.save(commit=False)
+            # user.set_password(password)
+            # user.role=User.CUSTOMER
+            # user.save()
+
+            # create a user user_create method
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            user=User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role=User.CUSTOMER
             user.save()
             return redirect('registerUser')
